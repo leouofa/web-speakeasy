17c29d6b7215b2f0e2c138864f3ab574e26585f6
Leonid Medovyy <leonid@storypro.io>
2023-02-21 12:43:24 -0700

    modified the message component to render messages

--- components/ChatRoom.tsx
+++ components/ChatRoom.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
+import { clientPusher } from '../serverPusher';
+import { Message } from '../typings';
+import MessageComponent from 'components/MessageComponent';
+
 function ChatRoom(){
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const channel = clientPusher.subscribe('messages');
+
+    channel.bind('new-message', async (data: Message) => {
+      if(messages?.find((message) => message.id === data.id )) return;
+
+      setMessages([data, ...messages])
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages, clientPusher])
 
   return (
     <div className="">
-      Chatroom
+      <h1>Hello World</h1>{messages?.count}
+      {(messages).map(message => (
+        <MessageComponent message={message} key={message.id} />
+      ))}
     </div>
+
   )
 }
 
