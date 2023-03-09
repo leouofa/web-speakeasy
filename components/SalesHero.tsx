2fd27a3de85cebef22b76445e6d25f81e541ec26
Leonid Medovyy <leonid@storypro.io>
2023-03-09 09:27:44 -0700

    updating navbar

--- components/SalesHero.tsx
+++ components/SalesHero.tsx
@@ -13,7 +13,7 @@ export default function SalesHero(){
 
  return(
    <section className="bg-gray-900">
-     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-32 lg:grid-cols-12">
        <div className="mr-auto place-self-center lg:col-span-7">
          <h1
            className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl mb-4"
@@ -32,9 +32,9 @@ export default function SalesHero(){
            Subscribe Now
            <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg">
-             <path fill-rule="evenodd"
+             <path fillRule="evenodd"
                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                   clip-rule="evenodd"></path>
+                   clipRule="evenodd"></path>
            </svg>
          </a>
        </div>
